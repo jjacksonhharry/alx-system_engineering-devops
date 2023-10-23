6bed3ba3bb2516c884a67a45a776f85019bee154
Harry Muriithi <hharryjjack@gmail.com>
2023-10-23 18:40:05 +0300

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,6 +8,10 @@ import sys
 
 
 def get_employee_todo_progress(employee_id):
+    """
+    Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress
+    """
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/todos?userId={employee_id}"

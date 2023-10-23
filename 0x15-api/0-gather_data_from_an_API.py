08d2ea35513b9e1fead65fb53dbdfbea897c7d5e
Harry Muriithi <hharryjjack@gmail.com>
2023-10-23 18:42:16 +0300

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,20 +17,16 @@ def get_employee_todo_progress(employee_id):
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
-        # Fetch employee data
         user_response = requests.get(user_url)
         user_data = user_response.json()
         employee_name = user_data['name']
 
-        # Fetch TODO list data
         todo_response = requests.get(todo_url)
         todo_data = todo_response.json()
 
-        # Calculate progress
         total_tasks = len(todo_data)
         completed_tasks = sum(1 for task in todo_data if task['completed'])
 
-        # Display progress information
         p_m = f"Employee {employee_name} is done with tasks
         ({completed_tasks}/{total_tasks}): "
         print(p_m)
@@ -47,6 +43,9 @@ def get_employee_todo_progress(employee_id):
 
 
 if __name__ == "__main__":
+    """
+    calling the main function
+    """
     if len(sys.argv) != 2:
         print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)

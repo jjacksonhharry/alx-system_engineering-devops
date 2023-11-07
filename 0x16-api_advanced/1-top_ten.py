70df73be8b57673a21726b413c12fe0a58f5d5e1
Harry Muriithi <hharryjjack@gmail.com>
2023-11-07 23:25:56 +0300

    task 1 trial 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,6 +10,9 @@ def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts for a given subreddit.
     """
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
     headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
